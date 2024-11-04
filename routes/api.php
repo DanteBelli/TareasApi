28f9c24a1a3302ae4841b264ae28115b8bfc4566
Bell <dante.be@hotmail.com>
2024-11-04 18:01:19 -0300

    ModificacionRutasApi+creacion modelo mcr

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,19 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::get('tareas', function() {
+    return 'prueba';
+});
+Route::get('tareas/{id}', function() {
+    return 'prueba';
+});
+Route::patch('tareas/{id}', function() {
+    return 'prueba';
+});
+Route::post('tareas', function() {
+    return 'prueba';
+});
+Route::delete('tareas/{id}', function() {
+    return 'prueba';
+});
